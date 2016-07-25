87432b31cf266f40dd86e9d69e84ac40b2c04be3
Ben Hallion <ben.hallion@aisquared.com>
2016-07-25 09:03:38 -0400

    escape newlines when stringifying code

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 'use strict';
 
 const
-    gulp       = require('gulp'),
-    handlebars = require('gulp-compile-handlebars'),
-    rename     = require('gulp-rename'),
-    readFile   = require('./lib/util/read-file');
-
-let sourceMap;
+    sourceMap     = {},
+    gulp          = require('gulp'),
+    handlebars    = require('gulp-compile-handlebars'),
+    rename        = require('gulp-rename'),
+    vinylMap      = require('vinyl-map'),
+    replace       = require('gulp-replace');
 
 gulp.task('createSourceMap', () => {
-    return readFile('./lib/page-observer.js')
-        .then(source => {
-            sourceMap = {
-                'observer-script' : source
-            };
-        });
+
+    let fileToString = vinylMap(file => {
+        sourceMap['observer-script'] = file.toString();
+    });
+
+    gulp.src('lib/page-observer.js')
+        .pipe(replace('\\', '\\\\'))
+        .pipe(fileToString)
 });
 
 gulp.task('build', ['createSourceMap'], () => {
